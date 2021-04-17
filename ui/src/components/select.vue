5419e56334f3e36e70bb45273ceb19b77ded6230
thestarweb <597577939@qq.com>
2021-04-17 17:17:52 +0800

    引入fonticon完善select控件

--- ui/src/components/select.vue
+++ ui/src/components/select.vue
@@ -1,11 +1,20 @@
 <template>
-	<div :class="['star-ui','star-ui-select',{'is-focus':isFocused}]" @click="isFocused=true">
+	<div :class="['star-ui','star-ui-select',{'is-focus':isFocused}]" @click="isFocused=!isFocused">
+		<!-- 原生组件 -->
 		<select :name="name" style="display: none"></select>
 		<input v-if="false" type="text" :value="1" class="star-ui star-ui-select--inner">
-		<div class="star-ui star-ui-container">{{this.showValue}}</div>
+		<!-- 内容显示 -->
+		<div class="star-ui star-ui-select--inner-content star-ui-container">{{this.showValue}}</div>
+		<!-- 后缀 -->
+		<div class="star-ui start-ui-select--inner-suffix star-ui-container">
+			<i class="star-ui-icon-font star-ui-icon-arrow-down"/>
+		</div>
+		<!-- 选项弹窗 -->
 		<base-popper :from-item="$el" :visible="isFocused" @out-click="isFocused=false">
-			<div class="star-ui-select--pooper">
-				<div v-for="item in valueMap" :key="item[0]" class="star-ui-container" @click="handleSelect(item[0])">{{item[1]}}</div>
+			<div class="star-ui star-ui-select--pooper" :style="{width:width+'px'}">
+				<div v-for="item in valueMap" :key="item[0]" :class="['star-ui','star-ui-select--option','star-ui-container',{
+					'star-ui-select--option-sel':item[0]==this.value
+				}]" @click="handleSelect(item[0])">{{item[1]}}</div>
 			</div>
 		</base-popper>
 	</div>
@@ -51,8 +60,17 @@ declare interface IObj {
 			type:String,
 			default:"value"
 		},
-
-
+	},
+	watch:{
+		isFocused(v){
+			if(v){
+				if(this.$el){
+					if(this.$el.clientWidth){
+						this.width=this.$el.clientWidth;
+					}
+				}
+			}
+		}
 	}
 })
 export default class SuSelect extends Vue {
@@ -82,9 +100,7 @@ export default class SuSelect extends Vue {
 		}
 		return this.value;
 	}
-	// mounted():void{
-	// 	this.$forceUpdate();
-	// }
+	private width=280;
 	handleSelect(value:string):void{
 		this.$emit("input",value);
 		this.isFocused=false;
@@ -93,23 +109,41 @@ export default class SuSelect extends Vue {
 </script>
 
 <style>
+/*基本样式*/
 .star-ui-select{
 	display: inline-block;
 	border: solid 1px var(--star-ui-input-border-color);
 	border-radius: var(--star-ui-border-radius);
+	display: flex;
+	flex-direction: row;
+	font-size: var(--star-ui-input-medium-font-size);
+	line-height: var(--star-ui-input-medium-font-size);
+	width:var(--star-ui-input-default-width);
 }
+/*选中时样式*/
 .star-ui-select:hover,.star-ui-select.is-focus{
 	border: solid 1px var(--star-ui-base-color);
 }
+/*内容显示区域占用全部剩余空间*/
+.star-ui-select--inner-content{
+	flex:1;
+}
+/*清除原生输入框的样式*/
 .star-ui-select--inner,.star-ui-select--inner:hover,.star-ui-select--inner:active,.star-ui-select--inner:focus{
 	border-width: 0px;
 	outline: none;
 	margin: 0;
 	padding: 0;
 }
+/*弹出层样式*/
 .star-ui-select--pooper{
-	background: #FFF;
+	background: var(--star-ui-backgorund-color);
 	border: solid 1px var(--star-ui-input-border-color);
 	border-radius: var(--star-ui-border-radius);
+}
+/*选中选项样式*/
+.star-ui-select--option-sel{
+	background: var(--star-ui-base-color);
+	color:var(--star-ui-backgorund-color);
 }
 </style>
