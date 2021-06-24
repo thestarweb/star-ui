86718140eb993d80e863176ae4f8640aa9c52f4b
thestarweb <597577939@qq.com>
2021-06-24 20:58:41 +0800

    从环境变量读取cdn地址

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	configureWebpack: config => {
+		if(process.env.CDN_LIST && process.env.NODE_ENV == "production"){
+			const CDNList=process.env.CDN_LIST.split("|");
+			let CDNURLs={};
+			if(!config.externals) config.externals = {};
+			CDNList.forEach((srcItem) => {
+				const item = srcItem.split("::");
+				config.externals[item[0]] = item[2] || item[0];
+				CDNURLs[item[0]] = item[1];
+			});
+			// config.externals.vue="Vue";
+			config.plugins.forEach((val) => {
+	            if (val instanceof HtmlWebpackPlugin) {
+	                // console.log(val)
+	                //let _ = val.options.templateParameters;
+					// val.options.cdn={
+					// 	Vue:"https://unpkg.com/vue@next"
+					// };
+					val.options.cdn=CDNURLs;
+	            }
+	        });
+		}
+	}
+}
