add11c638841e8feed7be2c2de45c0eab598e583
thestarweb <597577939@qq.com>
2021-11-20 22:48:15 +0800

    修复编译问题

--- ui/src/components/form/form/index.ts
+++ ui/src/components/form/form/index.ts
@@ -1,2 +1,2 @@
-export { default as SuForm } from './form.vue';
-export { default as SuFormItem } from './form-item.vue';
+export { default as SuForm } from "./form.vue";
+export { default as SuFormItem } from "./form-item.vue";
